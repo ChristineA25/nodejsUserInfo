8bb2d3190f8b2e416ce2da2bf03f20237a6cab9d
Ka Yung Au <Ka4.Au@live.uwe.ac.uk>
2026-01-12 08:28:09 +0000

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const indexRouter = require('./routes/index');
 
 const app = express();
 // Listen on Railway's PORT
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
